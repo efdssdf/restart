8f5b15ba48394ed6b1e0bf4459e555337502641b
efdssdf <efdssdf@gmail.com>
2018-09-25 15:23:35 +0800

    'init'

---
+++ script/restart.js
@@ -0,0 +1,35 @@
+var schedule = require("node-schedule");
+var request = require('request');
+var url_conf = require('../conf/url.json');
+var DomainModel = require('../model/Domain');
+var exec = require('child_process').exec;
+
+function restart() {
+    DomainModel.find(function (err,data) {
+        var cmdStr1 = 'pm2 restart tuitui'
+        var cmdStr2 = 'pm2 restart tuitui_1'
+        var cmdStr3 = 'pm2 restart tuitui_2'
+        var cmdStr4 = 'pm2 restart tuitui_3'
+        exec(cmdStr1, function(err,stdout,stderr){
+            console.log("restart tuitui")
+        })
+        exec(cmdStr2, function(err,stdout,stderr){
+            console.log("restart tuitui_1")
+        })
+        exec(cmdStr3, function(err,stdout,stderr){
+            console.log("restart tuitui_2")
+        })
+        exec(cmdStr4, function(err,stdout,stderr){
+            console.log("restart tuitui_3")
+        })
+    })
+}
+var rule = new schedule.RecurrenceRule();
+var times1 = [1,3,5];
+var times2 = [1];
+rule.dayOfWeek = times1;
+rule.hour = times2;
+var j = schedule.scheduleJob(rule, function () {
+    console.log('重启服务');
+    restart();
+});
