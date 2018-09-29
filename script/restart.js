abf975313773e276f036e9335a1db43644d4a75b
efdssdf <efdssdf@gmail.com>
2018-09-29 16:39:31 +0800

    'fix'

--- script/restart.js
+++ script/restart.js
@@ -2,27 +2,25 @@ var schedule = require("node-schedule");
 var exec = require('child_process').exec;
 
 function restart() {
-    DomainModel.find(function (err,data) {
-        var cmdStr1 = 'pm2 restart tuitui'
-        var cmdStr2 = 'pm2 restart tuitui_1'
-        var cmdStr3 = 'pm2 restart tuitui_2'
-        var cmdStr4 = 'pm2 restart tuitui_3'
-        exec(cmdStr1, function(err,stdout,stderr){
-            console.log("restart tuitui")
-        })
-        exec(cmdStr2, function(err,stdout,stderr){
-            console.log("restart tuitui_1")
-        })
-        exec(cmdStr3, function(err,stdout,stderr){
-            console.log("restart tuitui_2")
-        })
-        exec(cmdStr4, function(err,stdout,stderr){
-            console.log("restart tuitui_3")
-        })
+    var cmdStr1 = 'pm2 restart tuitui'
+    var cmdStr2 = 'pm2 restart tuitui_1'
+    var cmdStr3 = 'pm2 restart tuitui_2'
+    var cmdStr4 = 'pm2 restart tuitui_3'
+    exec(cmdStr1, function (err, stdout, stderr) {
+        console.log("restart tuitui")
+    })
+    exec(cmdStr2, function (err, stdout, stderr) {
+        console.log("restart tuitui_1")
+    })
+    exec(cmdStr3, function (err, stdout, stderr) {
+        console.log("restart tuitui_2")
+    })
+    exec(cmdStr4, function (err, stdout, stderr) {
+        console.log("restart tuitui_3")
     })
 }
 var rule = new schedule.RecurrenceRule();
-var times1 = [2,4,6];
+var times1 = [2, 4, 6];
 var times2 = [1];
 rule.dayOfWeek = times1;
 rule.hour = times2;
